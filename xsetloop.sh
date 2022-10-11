c6e5db3b7d0f68346342aeab06830a50c25b55b2
0x6775737461 <fabiogustavo@tuta.io>
2022-10-11 12:59:35 -0400

    pegando o valor literal do brilho da tela

--- xsetloop.sh
+++ xsetloop.sh
@@ -2,12 +2,32 @@
 
 get_date() {
    # informações sobre o horário atual
+
+   local info
    info=$(date +'%H:%M:%S')
 
    echo -e "$info"
 }
 
-func_names=('get_date')
+get_brightness() {
+   # valor literal do brilho da tela
+   # obs: funciona apenas no intel
+
+   #TODO: suporte a outras GPUs
+
+   local path
+   local info
+
+   path='/sys/class/backlight/intel_backlight/brightness'
+   info=$(<${path})
+
+   echo -e "$info"
+}
+
+func_names=(
+            'get_brightness'
+            'get_date'
+)
 
 while true; do
 
